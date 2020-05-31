94064c8950e703a12c4073888aaebfccce4b86ee
Don Ky <don.d.ky@gmail.com>
2020-05-30 23:30:19 -0400

    Remove sudo from install docker

--- provision/install-docker.sh
+++ provision/install-docker.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get -y install \
+apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -10,13 +9,13 @@ sudo apt-get -y install \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
 
-sudo usermod -G docker `whoami`
+usermod -G docker vagrant
