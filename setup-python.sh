2c876ba0b451e9fc382960e43fe03ff103019621
Don Ky <don.d.ky@gmail.com>
2021-06-21 00:01:29 -0400

    Check if pyenv dir exists, if it does then it's assumed we have pyenv installed

--- setup-python.sh
+++ setup-python.sh
@@ -2,6 +2,12 @@
 
 sudo apt-get -y install build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev libgdbm-compat-dev liblzma-dev libreadline-dev libncursesw5-dev libffi-dev uuid-dev
 
+PYENV_DIR=$HOME/.pyenv
+
+if [ -d "$PYENV_DIR" ];
+then
+	echo "Pyenv is already installed"
+else
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
@@ -11,3 +17,4 @@ pyenv global 3.9.5
 
 /home/vagrant/.pyenv/shims/pip install --upgrade pip
 /home/vagrant/.pyenv/shims/pip install --user pynvim
+fi
