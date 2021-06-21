c5d1337af1791873759717e02ba85bac858216b7
Don Ky <don.d.ky@gmail.com>
2021-06-21 00:15:42 -0400

    Setup ruby script

---
+++ setup-ruby.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash
+
+RUBY_VERSION=2.7.3
+
+rvm install $RUBY_VERSION
+rvm use $RUBY_VERSION
