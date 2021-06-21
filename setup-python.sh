b43890c8e5ceb8db1cfe7229878190d008aba3d9
Don Ky <don.d.ky@gmail.com>
2021-06-20 22:35:34 -0400

    Point to pip shim, Bump python version

--- setup-python.sh
+++ setup-python.sh
@@ -3,16 +3,11 @@
 sudo apt-get -y install build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev libgdbm-compat-dev liblzma-dev libreadline-dev libncursesw5-dev libffi-dev uuid-dev
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-
-pyenv install 3.7.5
-pyenv global 3.7.5
-
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
+pyenv install 3.9.5
+pyenv global 3.9.5
 
-pip install --upgrade pip
-pip install --user pynvim
+/home/vagrant/.pyenv/shims/pip install --upgrade pip
+/home/vagrant/.pyenv/shims/pip install --user pynvim
