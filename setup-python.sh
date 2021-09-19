1a0f08dc14e16de665d9918f79d32ea87ebc93cf
Don Ky <don.d.ky@gmail.com>
2021-09-19 13:25:00 -0400

    Clean up else block

--- setup-python.sh
+++ setup-python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -y install build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev libgdbm-compat-dev liblzma-dev libreadline-dev libncursesw5-dev libffi-dev uuid-dev
+sudo apt-get update && sudo apt-get -y install build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev libgdbm-compat-dev liblzma-dev libreadline-dev libncursesw5-dev libffi-dev uuid-dev
 
 PYENV_DIR=$HOME/.pyenv
 
@@ -8,13 +8,13 @@ if [ -d "$PYENV_DIR" ];
 then
 	echo "Pyenv is already installed"
 else
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+	export PYENV_ROOT="$HOME/.pyenv"
+	export PATH="$PYENV_ROOT/bin:$PATH"
 
-pyenv install 3.9.7
-pyenv global 3.9.7
+	pyenv install 3.9.7
+	pyenv global 3.9.7
 
-/home/vagrant/.pyenv/shims/pip install --upgrade pip
-/home/vagrant/.pyenv/shims/pip install --user pynvim
+	$HOME/.pyenv/shims/pip install --upgrade pip
+	$HOME/.pyenv/shims/pip install --user pynvim
 fi
