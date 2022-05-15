07408adb518a645303332e1fe4d32959e40f8f8d
Don Ky <don.d.ky@gmail.com>
2022-05-15 13:29:04 -0400

    Move provision scripts into .disabled when not activated

--- provision/setup-go.sh
+++ provision/setup-go.sh
@@ -1,1 +1,15 @@
-../setup-go.sh
+#!/bin/bash
+
+VERSION=1.17.1
+URL=https://dl.google.com/go/go$VERSION.linux-amd64.tar.gz
+
+GODIR=/usr/local/go
+
+if [ -d "$GODIR" ];
+then
+	echo "Go is already installed"
+else 
+	echo "Downloading Go $VERSION and extracting + installing"
+	sudo wget -q $URL -O - | tar -xz -C /tmp
+	sudo mv /tmp/go /usr/local/
+fi
