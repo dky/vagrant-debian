375fc3a488301f1ac5afb4e769fa7ad1b52b7b6e
vagrant <vagrant@sandbox>
2022-05-15 22:35:46 +0000

    Tweak nvim script, add more details

--- provision/.disabled/nvim.sh
+++ provision/.disabled/nvim.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+NVIM_VERSION=v0.7.0
+
 mkdir ~/bin ~/tmp
-wget -O ~/tmp/nvim.tar.gz  https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.tar.gz
-tar -xzvf ~/tmp/nvim.tar.gz -C ~/tmp/
-ln -s ~/tmp/nvim-linux64/bin/nvim ~/bin/
+wget -O ~/tmp/nvim.tar.gz  https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim-linux64.tar.gz
+
+tar -xzvf ~/tmp/nvim.tar.gz -C ~/tmp/ && ln -s ~/tmp/nvim-linux64/bin/nvim ~/bin/
+
+# Install vim-plug for nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
