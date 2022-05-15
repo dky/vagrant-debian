20a60c701265c5279c687d9d9157abee5f0d133d
vagrant <vagrant@sandbox>
2022-05-15 22:42:16 +0000

    Tweak docker installer for debian vs ubuntu

--- provision/.disabled/install-docker.sh
+++ provision/.disabled/install-docker.sh
@@ -9,11 +9,19 @@ apt-get -y install \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+
+if [  -n "$(uname -a | grep Ubuntu)" ]; then
+	apt-key fingerprint 0EBFCD88
+	add-apt-repository \
+		"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+		$(lsb_release -cs) \
+		stable"
+else
+	 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+	echo \
+		"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+		$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+fi
 
 apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io
