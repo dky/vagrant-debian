97eaa0a52358845fb21280eaa5e34c20fd60c118
Don Ky <don.d.ky@gmail.com>
2022-05-15 20:46:58 -0400

    Clean out tar after extract

--- provision/.disabled/nvim.sh
+++ provision/.disabled/nvim.sh
@@ -10,3 +10,5 @@ tar -xzvf ~/tmp/nvim.tar.gz -C ~/tmp/ && ln -s ~/tmp/nvim-linux64/bin/nvim ~/bin
 # Install vim-plug for nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+rm ~/tmp/nvim.tar.gz
