d895aac72d0f23e45e76c08a34dfe597675ba15a
vagrant <vagrant@sandbox>
2023-04-15 18:42:50 +0000

    Tweak nvim for arm appimage

--- provision/nvim.sh
+++ provision/nvim.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 NVIM_VERSION=v0.9.0
+
+
+if [[ $(uname -m) == "aarch64" ]]
+then 
+	wget -O ~/bin/nvim https://github.com/matsuu/neovim-aarch64-appimage/releases/download/v0.9.0/nvim-v0.9.0.appimage
+	chmod u+x ~/bin/nvim
+
+else
 
 mkdir ~/bin ~/tmp
 wget -O ~/tmp/nvim.tar.gz  https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim-linux64.tar.gz
@@ -12,3 +20,5 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 rm ~/tmp/nvim.tar.gz
+
+fi
